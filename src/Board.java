00ba7cc3660cc25b85047dd7226636066f5626de
aneumann20 <anita.neumann.14@sansano.usm.cl>
2015-12-01 12:43:12 -0300

    Agregue clase Board

---
+++ src/Board.java
@@ -0,0 +1,18 @@
+import lp.motor.Context;
+import lp.motor.MouseHandler;
+
+import java.awt.*;
+
+public class Board implements Context{
+
+
+    @Override
+    public void update(MouseHandler mouseHandler) {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}
