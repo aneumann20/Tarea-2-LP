6441eee27773fde93c4dcd80a5e9f46fb07a22d8
aneumann20 <anita.neumann.14@sansano.usm.cl>
2015-12-01 12:48:27 -0300

    Agregue clase Piece

---
+++ src/Piece.java
@@ -0,0 +1,16 @@
+import lp.motor.Context;
+import lp.motor.MouseHandler;
+
+import java.awt.*;
+
+public class Piece implements Context {
+    @Override
+    public void update(MouseHandler mouseHandler) {
+
+    }
+
+    @Override
+    public void render(Graphics graphics) {
+
+    }
+}
